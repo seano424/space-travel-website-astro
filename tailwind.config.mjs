1364b6824cb695c20a529ac6abbda6867f3d9d6e
seano424 <soreilly424@gmail.com>
2024-09-12 10:47:46 -0400

    adding google fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				dark: 'hsl(230, 35%, 7%)',
+				accent: 'hsl(231, 77%, 90%)',
+				white: 'hsl(0, 0%, 100%)',
+			}
+		},
+		fontFamily: {
+			"sans-cond": ['Barlow Condensed', ...defaultTheme.fontFamily.sans],
+			sans: ['Barlow', ...defaultTheme.fontFamily.sans],
+			serif: ['Bellefair', ...defaultTheme.fontFamily.serif],
+		},
 	},
 	plugins: [],
 }
